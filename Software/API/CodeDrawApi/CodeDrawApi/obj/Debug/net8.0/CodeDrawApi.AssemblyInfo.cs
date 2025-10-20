9645dacbc0139d85b220892bc27dcac43eefa93f
Erick Dharo <erickbandeiradharo@gmail.com>
2025-10-20 20:39:01 -0300

    Prototipo transformado para jsx, funcioando e validado. Pagina login iniciada

--- Software/API/CodeDrawApi/CodeDrawApi/obj/Debug/net8.0/CodeDrawApi.AssemblyInfo.cs
+++ Software/API/CodeDrawApi/CodeDrawApi/obj/Debug/net8.0/CodeDrawApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeDrawApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d24356faa9bfc246741d9c1afe7ee616c923c1d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeDrawApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeDrawApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
